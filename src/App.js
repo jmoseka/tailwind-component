f9efc926d1c5c465e4e56b5d141b721fb007ce87
jmoseka <jamylam3@gmail.com>
2023-08-04 21:12:28 +0300

    add new fontfamily

--- src/App.js
+++ src/App.js
@@ -12,74 +12,74 @@ function App() {
           <li> Desktop version- 5rows 4 columns </li>
           <li>Mobile version - 12rows 2 columns</li>
           <li>Source code <a class="underline underline-offset-2
-           hover:text-pink-500 decoration-solid font-semibold" href="https://github.com/jmoseka/tailwind-component">here</a></li>
+           hover:text-pink-500 decoration-solid font-semibold" target='_blank' href="https://github.com/jmoseka/tailwind-component/blob/main/src/App.js" rel="noreferrer">here</a></li>
 
         </ul>
 
         {/* <!--Grid container--> */}
         <div class="grid-container mt-8  m-auto h-full md:h-4/6 max-w-3xl">
 
-          <div class="h-full w-full px-2 py-2 mx-auto max-w-4xl text-white text-center
+          <div class="h-full w-full py-2 mx-auto max-w-4xl text-white text-center
      grid grid-cols-2 grid-rows-12 md:grid-cols-5 md:grid-rows-4 gap-2">
 
 
-            <div class="box-one bg-blue-500 row-span-2 md:col-span-1 md:row-span-2">
+            <div class="grid-box bg-blue-500 row-span-2 md:col-span-1 md:row-span-2">
               <span class="hidden md:block">row-span-2</span>
             </div>
 
 
-            <div class="box-one col-span-1 md:col-span-2 bg-white text-black">
+            <div class="grid-box col-span-1 md:col-span-2 bg-white text-black">
               <span class="hidden md:block">col-span-2</span>
             </div>
 
 
-            <div class="box-one bg-indigo-700 row-span-2 md:row-span-1 md:col-span-1">
+            <div class="grid-box bg-indigo-700 row-span-2 md:row-span-1 md:col-span-1">
               <span class="hidden md:block">col-span-1</span>
             </div>
 
 
-            <div class="box-one bg-red-500 row-span-2 col-span-1  md:col-span-1 md:row-span-2">
+            <div class="grid-box bg-red-500 row-span-2 col-span-1  md:col-span-1 md:row-span-2">
               <div class="hidden md:block">row-span-2</div>
               <div class="hidden md:block">col-span-1</div>
             </div>
 
 
-            <div class="box-one bg-yellow-500 row-span-1 col-span-1 md:row-span-2">
+            <div class="grid-box bg-yellow-500 row-span-1 col-span-1 md:row-span-2">
               <span class="hidden md:block">row-span-2</span>
             </div>
 
 
-            <div class="box-one bg-slate-600 col-span-2">
+            <div class="grid-box bg-slate-600 col-span-2">
               <span class="hidden md:block">col-span-2</span>
             </div>
 
 
-            <div class="box-one bg-cyan-700 row-span-2 md:row-span-1 md:col-span-1">
+            <div class="grid-box bg-cyan-700 row-span-2 md:row-span-1 md:col-span-1">
               <span class="hidden md:block">col-span-1</span>
             </div>
 
 
-            <div class="box-one bg-fuchsia-700  row-span-2">
+            <div class="grid-box bg-fuchsia-700  row-span-2">
               <span class="hidden md:block">row-span-2</span>
             </div>
 
 
-            <div class="box-one bg-purple-800 col-span-2">
+            <div class="grid-box bg-purple-800 col-span-2">
               <span class="hidden md:block">col-span-2</span>
             </div>
 
 
-            <div class="box-one bg-red-600 col-span-1 md:col-span-2">
+            <div class="grid-box bg-red-600 col-span-1 md:col-span-2">
               <span class="hidden md:block">col-span-2</span>
             </div>
 
 
-            <div class="box-one bg-lime-600 row-span-2 md:row-span-1 md:col-span-1">
+            <div class="grid-box bg-lime-600 row-span-2 md:row-span-1 md:col-span-1">
               <span class="hidden md:block">col-span-1</span>
             </div>
 
 
-            <div class="box-one bg-blue-500 md:col-span-1">
+            <div class="grid-box bg-blue-500 md:col-span-1">
               <span class="hidden md:block">col-span-1</span>
             </div>
 
