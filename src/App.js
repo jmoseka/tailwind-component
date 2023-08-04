446a7a51f649a5bacc43b62d6d07d00071ff25ba
jmoseka <jamylam3@gmail.com>
2023-08-04 20:41:26 +0300

    adjust top margin and add project link

--- src/App.js
+++ src/App.js
@@ -7,26 +7,14 @@ function App() {
       {/* <!-- Global Container --> */}
       <div class="container h-screen px-3 max-w-7xl">
 
-        <div class="desc text-white mt-4 flex flex-col justify-center items-center">
-          <p>12 grids / boxes </p>
-          <p>Source code <a class="underline underline-offset-2
-           hover:text-pink-500 decoration-solid font-semibold" href="#">here</a></p>
+        <ul class="desc text-white pt-6 flex flex-col items-start ">
+          <li>12 grid / boxes </li>
+          <li> Desktop version- 5rows 4 columns </li>
+          <li>Mobile version - 12rows 2 columns</li>
+          <li>Source code <a class="underline underline-offset-2
+           hover:text-pink-500 decoration-solid font-semibold" href="https://github.com/jmoseka/tailwind-component">here</a></li>
 
-          <div class="flex gap-32 md:gap-40 mt-3">
-            <div class="div">
-              <p>(Desktop version)</p>
-              <p>5 columns</p>
-              <p>4 rows</p>
-            </div>
-            <div class="div">
-              <p>(Mobile version)</p>
-              <p>2 colums </p>
-              <p>12 rows</p>
-
-            </div>
-          </div>
-
-        </div>
+        </ul>
 
         {/* <!--Grid container--> */}
         <div class="grid-container mt-8  m-auto h-full md:h-4/6 max-w-3xl">
